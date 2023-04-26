5c9636f1c1e43bde6fafdd3733e0d30e77de0674
NerokArtworks <nerokprodscontact@gmail.com>
2023-04-26 17:12:26 +0200

    Origins

--- src/main/java/com/book/shop/controladores/Librocontrolador.java
+++ src/main/java/com/book/shop/controladores/Librocontrolador.java
@@ -35,7 +35,7 @@ import com.book.shop.repositorios.UsuarioRepositorio;
 
 @RestController
 @RequestMapping("/libros")
-@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowedHeaders = "*")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowedHeaders = "*")
 public class Librocontrolador {
 
 	@Autowired
